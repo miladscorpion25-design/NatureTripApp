d314b9c91c083818e72b4e38e84d18c7454d5a5c
miladscorpion25-design <miladscorpion25@gmail.com>
2026-07-29 07:40:18 +0330

    Upgrade compileSdk and targetSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.naturetrip"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.naturetrip"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
