c250e8e680ba8129049942ee100193be7736cc2b
miladscorpion25-design <miladscorpion25@gmail.com>
2026-07-29 07:33:32 +0330

    Downgrade compileSdk and targetSdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.naturetrip"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.naturetrip"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
